f0b64c3dd504a99491b25e28aed05286b45b1320
Dijon Kock <dijon93@hotmail.com>
2015-06-05 12:13:06 +0200

    IP Week 1 - More Operations

---
+++ Interactive Python - P1/wk1-more-operations.py
@@ -0,0 +1,12 @@
+# More Operations
+
+# Remainder and Modular Arithmetic
+# - Standard long division yields a quotient and a remainder.
+# - The integer division operator // computes the quotient.
+# - The operator % computes the remainder.
+# - For any integers a and b, a == b * (a // b) + (a % b).
+# - In Python, a % b always returns an answer that is between 0 and b (even if a and/or b is negative).
+# - Remainders and modular arithmetic are very useful in games for the purpose
+# - of "wrapping" the canvas,
+# - i.e; causing objects that pass off of one side of the canvas to reappear on
+# - the opposite side of the canvas.
