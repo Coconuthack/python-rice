29854b82064e671a9c90eaf3937ea24dd24a5f03
Dijon Kock <dijon93@hotmail.com>
2015-06-07 02:35:57 +0200

    PyIP, W1, Logics

--- Interactive Python - P1/wk1-logical-ieks.py
+++ Interactive Python - P1/wk1-logical-ieks.py
@@ -277,3 +277,104 @@ print "Ex. 5:", (4 <= 4) == (5 >= 3)
 # It is especially easy to forget to use two equals signs
 #	when testing mathematical expressions
 #print "Error:", 3 * 4 = 4 * 3
+
+
+#--------------------------------------
+# Conditional Statements — Conditionals
+
+# Conditional statements are compound statements consisting one or more clauses
+#   headed by the keywords if, elif, and else.
+# Each if or elif clause is followed by a Boolean expression and a colon :.
+# If the Boolean expression for a clause is True, the body of the clause is executed.
+
+# Logic and Comparisons
+# if-elif-else
+
+
+# You can control which statements of your program execute
+#	using if, elif, and else statements. They consist of the
+#	keywords if, elif (else-if), or else, a boolean or boolean
+#	expression (for if and elif), a colon, and then some
+#	code in the body
+
+# Change the values of the following variables to see how they
+#	affect which statements are printed.
+
+# An 'if' statement can be followed by any number of 'elif'
+#	statements, followed by 0 or 1 'else' statement. 'if'
+#	statements can also be followed by other 'if' statements
+
+# Logic and Comparisons
+# Examples
+
+
+# Utilizing if-elif-else statements greatly expands the types
+#	of functions you can make. Note that the test cases try
+#	to test all of the different possibilities.
+
+# Absolute value
+
+def absolute_value(num):
+    if num < 0:
+        num = num * -1
+    return num
+
+print "Absolute Value:", absolute_value(9)
+print "Absolute Value:", absolute_value(-4)
+print "Absolute Value:", absolute_value(0)
+print
+
+# Number of real solutions to a quadratic equation
+
+def num_solutions(discriminant):
+    if discriminant > 0:
+        return 2
+    elif discriminant < 0:
+        return 0
+    else:
+        return 1
+
+print "Number of Solutions:", num_solutions(1)
+print "Number of Solutions:", num_solutions(-1)
+print "Number of Solutions:", num_solutions(0)
+print
+
+# Prints the type of triangle given the largest angle
+#	measurement in degrees
+
+def print_triangle_type(degrees):
+    if degrees > 90 and degrees < 180:
+        print "Triangle is obtuse!"
+    elif degrees == 90:
+        print "Triangle is right!"
+    elif degrees < 90 and degrees > 0:
+        print "Triangle is acute!"
+    else:
+        print "Triangle does not exist!"
+
+print_triangle_type(137)
+print_triangle_type(90)
+print_triangle_type(54)
+print_triangle_type(-1)
+print_triangle_type(180)
+print
+
+# Returns True if a triangle can be made with the given
+#	side lengths, False otherwise.
+#	(if the two smallest sides add up to be greater than
+#	 the largest one)
+
+def is_triangle(side_1, side_2, side_3):
+    max_side = max(side_1, side_2, side_3)
+    if max_side == side_1:
+        return (side_2 + side_3) > side_1
+    elif max_side == side_2:
+        return (side_1 + side_3) > side_2
+    else:
+        return (side_1 + side_2) > side_3
+
+print "Triangle:", is_triangle(3, 4, 5)
+print "Triangle:", is_triangle(5, 3, 4)
+print "Triangle:", is_triangle(4, 5, 3)
+print "Triangle:", is_triangle(1, 1, 2)
+print "Triangle:", is_triangle(5, 6, 19)
