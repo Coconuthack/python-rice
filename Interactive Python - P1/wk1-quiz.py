3a5c65d9b19930c41956e005a8e00ed09d0711c5
Dijon Kock <dijon93@hotmail.com>
2015-06-07 02:30:06 +0200

    PyIP, W1, Quiz Logic and conditionals

--- Interactive Python - P1/wk1-quiz.py
+++ Interactive Python - P1/wk1-quiz.py
@@ -38,3 +38,18 @@ print polygon_area(5,7)
 
 #Once the code has been fully corrected, it should print out two numbers. The
 #first should be 1.09888451159.
+#from:
+    # define project_to_distance(point_x point_y distance):
+    #     dist_to_origin = math.square_root(pointx ** 2 + pointy ** 2)
+    #      scale == distance / dist_to_origin
+    #     print point_x * scale, point_y * scale
+    #
+    # project-to-distance(2, 7, 4)
+
+import math
+def project_to_distance(point_x, point_y, distance):
+    dist_to_origin = math.sqrt(point_x ** 2 + point_y ** 2)
+    scale = distance / dist_to_origin
+    print point_x * scale, point_y * scale
+
+project_to_distance(2, 7, 4)
